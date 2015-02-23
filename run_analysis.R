2552f7fad7b09d9e859214bd31587ebb00ba6a98
Mariya Atanasova <mariya@microsoft.com>
2015-02-22 19:28:05 -0800

    Creating a tidy data set and saving it locally

--- run_analysis.R
+++ run_analysis.R
@@ -3,6 +3,8 @@
 #2/21/2015
 ###
 
+library(dplyr)
+
 setwd("C:/coursera/CleaningData")
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(url, "CleaningDataAssignment.zip")
@@ -18,13 +20,34 @@ featuresNames <- features$V2
 ## reading and labeling the training data
 trainName <- "train/X_train.txt"
 trainData <- read.table(trainName, stringsAsFactors=FALSE)
-names(trainData) <- featuresNames
+#names(trainData) <- featuresNames
 
 ## reading and labeling the test data
 testName <- "test/X_test.txt"
 testData <- read.table(testName, stringsAsFactors=FALSE)
-names(testData) <- featuresNames
+#names(testData) <- featuresNames
 
 ##merging the train and test data
 mergedData <- rbind(trainData, testData)
 head(mergedData)
+
+### Extracts only the measurements on the mean and standard deviation for each measurement. 
+meanNames <- grep("mean", featuresNames)
+stdNames <- grep("std", featuresNames)
+meanAndStdNames <- featuresNames[c(meanNames, stdNames)]
+
+###making the names clean by removing _, -, () , and white space)
+cleanMeanAndStdNames <- gsub("_", "", meanAndStdNames);
+cleanMeanAndStdNames <- gsub("-", "", cleanMeanAndStdNames);
+cleanMeanAndStdNames <- gsub("\\(", "", cleanMeanAndStdNames);
+cleanMeanAndStdNames <- gsub("\\)", "", cleanMeanAndStdNames);
+
+
+###creating the clean dataset
+meanAndSDTsubset <- select(mergedData, meanNames, stdNames)
+names(meanAndSDTsubset) <- cleanMeanAndStdNames
+head(meanAndSDTsubset)
+
+###saving the dataset:
+write.table(meanAndSDTsubset, file="TidyDataset.txt", row.names=FALSE)
+
